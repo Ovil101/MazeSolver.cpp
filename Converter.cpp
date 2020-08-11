6e08a3352d395e68f86d27b8ffd3201f1c77cb8c
ovil101 <rlioy1211@gmail.com>
2020-08-11 14:08:02 -0400

    Started using pointers to simplify logic

--- Converter.cpp
+++ Converter.cpp
@@ -33,26 +33,23 @@ std::vector<std::vector<Node>> Converter::to2Darray(){
     const cv::Vec3b BLACK(0, 0, 0);
     const cv::Vec3b WHITE(255,255,255);
     for (int i = 0; i < width; i++){
-        std::vector<Node> row = nodes[i];
+        std::vector<Node> *row = &nodes[i];
         for (int j = 0; j < height; j++){
             cv::Vec3b pixel = img.at<cv::Vec3b>(i,j); // in BGR format
             if (pixel == BLACK){ // thank God C++ has operator overloading
-                row[j].setIsWall(true); // segfault
+                (*row)[j].setIsWall(true);
             }
             else if (pixel == WHITE){
-                row[j].setIsWall(false);
+                (*row)[j].setIsWall(false);
             }
             else{
                 throw std::invalid_argument("Found non white or black pixel in image");
             }
         }
-        
-        nodes.erase(nodes.begin() + i); // remove old row
-        nodes.insert(nodes.begin() + i, row); // add new row
     }
 
     for (int row = 0; row < width; row++){
-        std::vector<Node> vec = nodes[row];
+        std::vector<Node> *vec = &nodes[row];
         for (int col = 0; col < width; col++){
             try{
                 
